bb8dea0ec75efa344618e58610ae384374204e39
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-10-04 03:45:14 -0700

    finish requirement of uploading file to a local directory and display directory as html

--- src/main/java/com/comp3940/assignment/UploadServer/UploadServlet.java
+++ src/main/java/com/comp3940/assignment/UploadServer/UploadServlet.java
@@ -2,20 +2,29 @@ package com.comp3940.assignment.UploadServer;
 
 import java.io.*;
 import java.time.Clock;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.lang.reflect.*;
 
 import com.comp3940.assignment.Dao.FormDao;
+import com.comp3940.assignment.Exceptions.FileTooLargeException;
 import com.comp3940.assignment.Utils.ConnectionManager;
+import com.comp3940.assignment.Utils.FileSystemHandler;
 import com.comp3940.assignment.Utils.HtmlExtractor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 
 @WebServlet(name = "UploadServlet", value = "")
 @MultipartConfig
 public class UploadServlet extends HttpServlet {
+   private static final long MAX_FILE_SIZE = 16 * 1024 * 1024;
    private final HtmlExtractor extractor = new HtmlExtractor();
+   private ObjectMapper mapper = new ObjectMapper();
    private FormDao formDao;
    public void init() {
       formDao = new FormDao();
@@ -30,10 +39,40 @@ public class UploadServlet extends HttpServlet {
          String caption = request.getParameter("caption");
          String date = request.getParameter("date");
          Part file = request.getPart("file");
-         formDao.insertFormData(caption, date, file);
+
+         if (file.getSize() >= MAX_FILE_SIZE) throw new FileTooLargeException("File is greater than " + MAX_FILE_SIZE);
+//         formDao.insertFormData(caption, date, file);
+         FileSystemHandler.saveToUploads(caption, date, file, request);
+         sendListing(request, response);
 
       } catch (Exception e) {
          System.err.println(e.getMessage());
       }
+   }
+
+   private void sendListing(HttpServletRequest request, jakarta.servlet.http.HttpServletResponse response) throws IOException {
+      File dir = new File(request.getServletContext().getRealPath("/uploads"));
+      String[] dirFiles = dir.list();
+      Arrays.sort(dirFiles);
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      String html = extractor.getHtml(request, "Directory");
+
+      // option 1: send html directly to front end
+      String populatedHtml = addDirectoryToHtml(html, dirFiles);
+      response.setContentType("text/html");
+      response.getWriter().println(populatedHtml);
+
+      // option 2: send json to front end, and let front end deal with populating
+//      String json = mapper.writeValueAsString(dirFiles);
+//      response.getWriter().write(json);
+   }
+
+   private String addDirectoryToHtml(String html, String[] directoryList) {
+      StringBuilder htmlDirFiles = new StringBuilder();
+      for (String file : directoryList) {
+         htmlDirFiles.append("<h2>").append(file).append("</h2><br>\n");
+      }
+      return html.replace("directoryHere", htmlDirFiles);
    }
 }
--- src/main/java/com/comp3940/assignment/Utils/FileSystemHandler.java
+++ src/main/java/com/comp3940/assignment/Utils/FileSystemHandler.java
@@ -1,17 +1,37 @@
 package com.comp3940.assignment.Utils;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.Part;
 
 import java.io.*;
+import java.time.Clock;
 
 public class FileSystemHandler {
-    public static void saveToUploads(String caption, String date, Part filePart) throws IOException {
-        OutputStream outputStream = null;
-        InputStream inputStream = filePart.getInputStream();
+    public static void saveToUploads(String caption, String date, Part filePart, HttpServletRequest request) {
+        try {
+            InputStream inputStream = filePart.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] content = new byte[1];
+            int bytesRead = -1;
+            while( ( bytesRead = inputStream.read( content ) ) != -1 ) {
+                baos.write( content, 0, bytesRead );
+            }
+            OutputStream outputStream = new FileOutputStream(createName(caption, date, filePart, request));
+            baos.writeTo(outputStream);
+            outputStream.close();
 
+        } catch(Exception ex) {
+            System.err.println(ex);
+        }
     }
 
-    private String combineName(String caption, String date, Part filePart) {
-        return  caption + "+" + date + "+" + filePart.getSubmittedFileName();
+    private static String createName(String caption, String date, Part filePart, HttpServletRequest request) {
+        // calculate time
+        Clock clock = Clock.systemDefaultZone();
+        long milliseconds = clock.millis();
+
+        // get filepath to uploads
+        String filepath = request.getServletContext().getRealPath("/uploads/");
+        return filepath + caption + "+" + date + "-" + milliseconds + "+" + filePart.getSubmittedFileName();
     }
 }
