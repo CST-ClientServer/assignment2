596c2e873d5cbdc84273e4a7929cbe27668158b3
Jason Chow <jchow149@my.bcit.ca>
2024-10-04 08:35:29 -0700

    REFACTOR to serve form.html

--- assignment2-client/src/App.js
+++ assignment2-client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import FileUploadForm from './components/FileUploadForm';
+import FormPage from "./components/FormPage";
+// import FileUploadForm from './components/FileUploadForm';
 
 function App() {
   return (
       <div className="App">
-        <h1>File Upload</h1>
-        <FileUploadForm />
+          <FormPage/>
       </div>
+      // <div className="App">
+      //   <h1>File Upload</h1>
+      //   <FileUploadForm />
+      // </div>
   );
 }
 
---
+++ assignment2-client/src/components/FormPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FormPage = () => {
+    return (
+        <div>
+            <h1>Upload Form</h1>
+            <iframe
+                src="http://localhost:8082/assignment_war/form.html"
+                width="100%"
+                height="500px"
+                title="Upload Form"
+            />
+        </div>
+    );
+};
+
+export default FormPage;
