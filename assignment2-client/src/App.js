8f5d3f9fc4e18bf6967bece95a250cc0657e0654
Jason Chow <jchow149@my.bcit.ca>
2024-10-06 03:20:54 -0700

    REFACTOR app.js

--- assignment2-client/src/App.js
+++ assignment2-client/src/App.js
@@ -81,9 +81,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>File Upload App</h1>
-                <h3>Upload a file along with caption and date</h3>
-
+                <h1>File Upload React App</h1>
                 <div>
                     <input type="file" onChange={this.onFileChange} />
                     <br />
@@ -104,7 +102,6 @@ class App extends Component {
                     <br />
                     <button onClick={this.onFileUpload}>Upload!</button>
                 </div>
-
                 {this.fileData()}
             </div>
         );
