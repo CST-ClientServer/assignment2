ce9df0480735f17ac782884cd07a2660d3004445
Jonathan <122506441+jwl0724@users.noreply.github.com>
2024-10-06 01:14:55 -0700

    add file reference

--- assignment2-app/app/src/main/java/org/assignment2app/Activity.java
+++ assignment2-app/app/src/main/java/org/assignment2app/Activity.java
@@ -1,14 +1,17 @@
 package org.assignment2app;
 
 import java.io.*;
+
 public class Activity {
+   private static final String imagesDirectoryPath = "app/images/";
+   private static final String imageFilename = "life.gif";
    public static void main(String[] args) throws IOException {
        new Activity().onCreate();
     }
    public Activity() {
    }
    public void onCreate() {
-      AsyncTask UploadAsyncTask = new UploadAsyncTask().execute(); 
+      AsyncTask UploadAsyncTask = new UploadAsyncTask(imagesDirectoryPath + imageFilename).execute();
       System.out.println("Waiting for Callback");
       try { 
          BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
--- assignment2-app/app/src/main/java/org/assignment2app/UploadAsyncTask.java
+++ assignment2-app/app/src/main/java/org/assignment2app/UploadAsyncTask.java
@@ -1,10 +1,16 @@
 package org.assignment2app;
 
 public class UploadAsyncTask extends AsyncTask {
+   private final String filepath;
+   public UploadAsyncTask(String filename) {
+      this.filepath = filename;
+   }
+
    protected void onPostExecute(String result) {
       System.out.println(result);
    }
+
    protected String doInBackground() {
-     return new UploadClient().uploadFile();
-   }  
+     return new UploadClient().uploadFile(filepath);
+   }
 }
